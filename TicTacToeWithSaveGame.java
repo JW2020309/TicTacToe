06056d55d9be8efef784e7779102e38a97b5494d
Jerilyn Widjaja <67321969+jerilynwidjaja@users.noreply.github.com>
2023-10-20 16:29:21 +0800

    Update TicTacToeWithSaveGame.java

--- TicTacToeWithSaveGame.java
+++ TicTacToeWithSaveGame.java
@@ -7,36 +7,6 @@ import java.io.PrintStream;
  * TicTacToeWithSaveGame is a subclass of TicTacToe that supports saving the game history.
  * All Exception shall be handled in try-catch, without any remedial actions and prints no message,
  * such that the normal game play is not affected.
- * 
- * Hint: use System.out redirection strategy
- * 
- * @author pffung
- * 
- *** STUDENTS SHOULD FINISH THIS CLASS PROPERLY TO MAKE IT WORK!
- * 
- * CSCI1130 Java Assignment 6 TicTacToe
- *
- * Remark: Name your class, variables, methods, etc. properly. 
- * You should write comment for your work and follow good styles.
- *
- * I declare that the assignment here submitted is original except for source
- * material explicitly acknowledged, and that the same or closely related
- * material has not been previously submitted for another course. I also
- * acknowledge that I am aware of University policy and regulations on honesty
- * in academic work, and of the disciplinary guidelines and procedures
- * applicable to breaches of such policy and regulations, as contained in the
- * website.
- *
- * University Guideline on Academic Honesty:
- *     http://www.cuhk.edu.hk/policy/academichonesty
- * Faculty of Engineering Guidelines to Academic Honesty:
- *     https://www.erg.cuhk.edu.hk/erg/AcademicHonesty
- *
- * Section     : CSCI1130B
- * Student Name: Jerilyn Widjaja
- * Student ID  : 1155172259
- * Date        : November 29, 2022
- *
  */
 public class TicTacToeWithSaveGame extends TicTacToe {
     
@@ -49,8 +19,6 @@ public class TicTacToeWithSaveGame extends TicTacToe {
      */
     public TicTacToeWithSaveGame() 
     {
-        /* Students' work here */
-        /* Just a few lines of code; all the rest are handled in the superclass */
         try {
             System.setOut(new PrintStream(new File("ttt.txt")));
         } catch (Exception e) {
